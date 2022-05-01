ae471b67efab51ce4fdda9a57f57dc7b65a58201
Vivian Yim <keroviv86@gmail.com>
2022-04-30 21:26:51 -0500

    making a form

--- src/components/WineList.tsx
+++ src/components/WineList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { WineData } from "../model";
 import WineCard from "./WineCard";
 import './components.css'
@@ -7,8 +7,32 @@ interface WineListProps {
   data: WineData[];
 }
 
+interface formDataProps {
+    Name: string;
+    Producer: string;
+    Region: string;
+    Country: string;
+    Varietal: string;
+    Image: string;
+}
+
 const WineList: React.FC<WineListProps> = ({ data }) => {
   console.log(data);
+  const [form, setForm] = useState<boolean>(false)
+  const [formData, setFormData] = useState<formDataProps>({
+      Name:"",
+      Producer:"", 
+      Region:"",
+      Country:"",
+      Varietal: "",
+      Image: ""
+
+  })
+
+  function handleForm(){
+      setForm(!form)
+
+  }
   let cards = data.map((card) => (
     <WineCard
       id={card.id}
@@ -20,7 +44,34 @@ const WineList: React.FC<WineListProps> = ({ data }) => {
       image={card.image}
     />
   ));
-  return <div>{cards}</div>;
+  return (
+      <div>
+          <div>{cards}</div>
+          {form ?
+          <>
+           <button onClick = {handleForm}>Hide Form</button>
+            <form >
+                <label>Name<input type="text" name="name" placeholder="name" /></label> 
+                <label>Producer<input type="text" name="name" placeholder="producer" /></label> 
+                <label>Region<input type="text" name="name" placeholder="region" /></label>
+                <label>Country<input type="text" name="name" placeholder="country" /></label>
+                <label>Varietal<input type="text" name="name" placeholder="varietal" /></label>
+                <label>Image<input type="text" name="image" placeholder="image url" /></label>
+                <button type="submit">Add Wine Card</button>
+            </form> 
+          </>
+            :
+            <>
+                <button onClick = {handleForm}>Add New Wine Card</button>
+            
+            </>
+            } 
+
+      </div>
+
+      
+    
+  ) 
 };
 
 export default WineList;
